65a96f52f751ae8ea1d99bed1f8374a125cf1c93
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-07 15:00:54 +0200

    changed foreign key

--- db/migrate/20170907121610_create_photos.rb
+++ db/migrate/20170907121610_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.1]
   def change
     create_table :photos do |t|
-      t.references :event, foreign_key: true
+      t.references :events, foreign_key: true
       t.string :image
 
       t.timestamps
